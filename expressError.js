52eefb5952d8f59db312a62ea14e9744a466e496
Hunter <hvanlear@gmail.com>
2021-03-05 13:20:32 -0500

    inital commit

---
+++ expressError.js
@@ -0,0 +1,12 @@
+// Error handling middleware to extend normal JS error
+
+class ExpressError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    console.error(this.stack);
+  }
+}
+
+module.exports = ExpressError;
